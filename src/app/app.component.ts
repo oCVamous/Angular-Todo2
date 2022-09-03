3a52cfed26753f8633badda8a8428f717c44c15a
Patrick Sterz <82339311+oCVamous@users.noreply.github.com>
2022-09-03 22:28:57 +0200

    commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { Component, Inject } from '@angular/core';
-// import { DialogAddTodoComponent } from './dialog-add-todo/dialog-add-todo.component';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { DialogAddTodoComponent } from './dialog-add-todo/dialog-add-todo.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { TodosService } from './todos.service';
-
-import { DialogExampleComponent } from './dialog-example/dialog-example.component';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +6,5 @@ import { DialogExampleComponent } from './dialog-example/dialog-example.componen
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
-  constructor(public dialog : MatDialog, public todosService: TodosService) {}
-  openDialog() {
-    const dialogRef = this.dialog.open(DialogAddTodoComponent);
-}
+  title = 'todoApp2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import {MatButtonModule} from '@angular/material';
-import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatDialogModule} from '@angular/material/dialog';
-import { DialogExampleComponent } from './dialog-example/dialog-example.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import { DialogAddTodoComponent } from './dialog-add-todo/dialog-add-todo.component';
-
-
-import { FormsModule } from '@angular/forms';
-
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DialogExampleComponent,
-    DialogAddTodoComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatFormFieldModule,
     FormsModule,
-    MatInputModule,
-    MatSelectModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
